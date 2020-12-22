05b44d74221899c7fcf1d3af63a48c7d53c48a1e
mkolata <marlon.kolata@t-online.de>
2020-12-22 01:18:30 +0100

    Added Mobile Experience

---
+++ src/components/UserList.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+//import Divider from '@material-ui/core/Divider';
+import { Typography } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    list: {
+        marginTop: 0,
+		marginBottom: 10,
+    },
+    listitem: {
+        marginBottom: -10,
+        marginLeft: -10,
+    }
+}));
+
+export default function MessageBox(props) {
+    const classes = useStyles();
+
+    function generateList(props) {
+        let mapCounter = 0;
+        let list = ["Test1-PC", "Test2-PC"].map((item) => (
+            <ListItem className={classes.listitem}>
+                <ListItemText primary={item} />
+            </ListItem>
+        ))
+        return list;
+    }
+    return (
+            <List className={classes.list} dense={true} disablePadding={true}>
+                {generateList()}
+            </List>
+    );
+}
