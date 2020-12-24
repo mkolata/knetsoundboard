bfdc122c28d5ebc79b1886020f596c3a6ada547a
mkolata <marlon.kolata@t-online.de>
2020-12-24 02:22:26 +0100

    Fix Burger Menu

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 import AppBar from './AppBar'
-import Drawer from './Drawer'
 export default function Main() {
-
-const [drawer, setDrawer] = useState(false);
 
 return(
   <div>
-    <AppBar onClick={() => setDrawer(!drawer)}/>
-    <Drawer drawer={drawer} onClick={() => setDrawer(!drawer)} />
+    <AppBar />
   </div>
 );
 
