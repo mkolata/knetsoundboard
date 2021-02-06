950175fd7d8124fe495856ae930aff487bede930
ZerNora <sahin-binici@hotmail.com>
2021-02-06 21:42:59 +0100

    Add file validation implementation

--- src/components/SubpageUpload/file-validation.js
+++ src/components/SubpageUpload/file-validation.js
@@ -4,8 +4,26 @@ class FileValidation {
 
     validate(file) {
 
-        return('blub');
-    }
+        var reader = new FileReader();
 
+        reader.onload = function (event) {
+
+            var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+            audioContext.decodeAudioData(event.target.result, function (buffer) {
+
+                if (buffer.duration > 0 && buffer.duration < 90) {
+                    return '0';
+                } else {
+                    return '2';
+                }
+            });
+        };
+
+        reader.onerror = function (event) {
+            return '1';
+        };
+
+        reader.readAsArrayBuffer(file);
+    }
 }
 export default new FileValidation();
